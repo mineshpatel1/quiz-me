fc1c6cc7871f16fe4e7b24905d553e6fc8892001
mineshpatel1 <nesh.patel1@gmail.com>
2019-05-26 19:23:21 +0000

    Added request fetching to the server

--- server/routes/friends.js
+++ server/routes/friends.js
@@ -7,8 +7,13 @@ const utils = require(__dirname + '/../api/utils.js');
 
 router.get('/friends', (req, res, next) => {
   if (!req.session.user) return next(new Error("User is not logged in."));
-  friends.get(req.session.user.id)
-    .then(friends => { return utils.response(res, { friends }) })
+  let _getFriends = friends.get(req.session.user.id);
+  let _getRequests = friends.getRequests(req.session.user.id);
+
+  Promise.all([_getFriends, _getRequests])
+    .then([_friends, _requests] => { 
+      return utils.response(res, { friends: _friends, requests: _requests });
+    })
     .catch(next);
 });
 
@@ -16,6 +21,7 @@ router.post('/friends/request', (req, res, next) => {
   let data = req.body;
   if (!req.session.user) return next(new Error("User is not logged in."));
   if (!data.email) return next(new Error("Friend's Email must be specified."));
+  if (req.session.user.email == data.email) return next(new Error("Can't be friends with yourself."));
   users.getFromEmail(data.email)
     .then(friend => {
       if (!friend) return next(new Error(data.email + " doesn't play QuizMe."))
